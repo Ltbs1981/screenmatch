7daad16c606e5b7fc7b5ef36052b8e8ef8441376
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-11 13:11:31 -0300

    feito os construtores

--- src/Principal.java
+++ src/Principal.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+
+
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -25,18 +25,17 @@ public class Principal {
         //meuFilme.totalDeAvaliacoes = 1;
         //System.out.println(meuFilme.pegaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
-        lost.setEpisodiosPorTemporada(10);
+        lost.setEpisodioPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+
+        Filme outroFilme = new Filme("Avatar", 2023);
+
+
         outroFilme.setDuracaoEmMinutos(200);
         outroFilme.exibeFichaTecnica();
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -54,10 +53,10 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
 
-        var filmeDoPaulo = new Filme();
-        filmeDoPaulo.setNome("Dog ville");
+        var filmeDoPaulo = new Filme("Dog Ville", 2003);
+        //filmeDoPaulo.setNome("Dog ville");
         filmeDoPaulo.setDuracaoEmMinutos(200);
-        filmeDoPaulo.setAnoDeLancamento(2003);
+        //filmeDoPaulo.setAnoDeLancamento(2003);
         filmeDoPaulo.avalia(10);
         ArrayList<Filme> listaDeFilmes = new ArrayList<>();
 listaDeFilmes.add(filmeDoPaulo);
